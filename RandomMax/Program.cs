20fec72534da486faa81c39330f223d46c1571a2
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-02-25 20:07:38 -0600

    Not Correct

--- RandomMax/Program.cs
+++ RandomMax/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace RandomMax
 {
     public static class Program
@@ -7,7 +8,32 @@ namespace RandomMax
         public static void Main()
         {
             // TODO: Generate a list of 100 random integers between 0 and 1000 and ouput the maximum value.
+            int randomInt = 0;
+            
+            int[] randomNums = new int[100];
 
+            for (int i = 0; i < 100; i++)
+            {
+                randomInt = new Random().Next(0, 1000);
+                //Console.WriteLine(i);
+                //Console.WriteLine(randomInt);
+                randomNums = new int[] { randomInt };
+                int maxValue = randomNums[0];
+                for (int j = 0; j < randomNums.Length; j++)
+                {
+                    if (maxValue < randomNums[j])
+                    {
+                        maxValue = j;
+                        Console.WriteLine(j);
+                    }
+
+
+                }
+
+
+            }                             
+                          
+            
             Console.ReadLine();
         }
     }
